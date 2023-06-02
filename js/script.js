cd1ce6910a920fe4323711bcd0630760ff428fcd
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-06-02 09:37:46 +0300

    change js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const numbers = [5, 8, -1, '-6', 11, 'ololo', '-2']
 let sum = 0
-
 for (let i = 0; i < numbers.length; i++) {
-   if (isNaN(numbers[i]) == true) { continue }
+   if (!Number(numbers[i])) { continue }
    else { sum += Number(numbers[i]) }
 }
-alert('Sum is ' + sum);  
+alert('Sum is ' + sum)
